4d0c10ce876ff7b339fc83ebc926fb0fe6ef4bc9
wwb <wwb@gmail.com>
2026-04-28 13:37:05 +0800

    Extract DSL expression AST types

--- quickjs-hook/src/jsapi/java/java_hook_api/managed_dex_builder/dsl/ast.rs
+++ quickjs-hook/src/jsapi/java/java_hook_api/managed_dex_builder/dsl/ast.rs
@@ -1,4 +1,4 @@
-use super::{DslCallStmt, DslFieldStmt, IfCmpOp};
+use super::{DslCallStmt, DslFieldStmt, DslIntBinOp, DslValue, IfCmpOp};
 
 pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) struct DslProgram {
     pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) stmts: Vec<DslStmt>,
@@ -147,75 +147,6 @@ pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) enum DslOrigArgs 
 }
 
 #[derive(Clone)]
-pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) enum DslValue {
-    Target(DslTarget),
-    String(String),
-    Int(i16),
-    Bool(bool),
-    Null,
-    UnaryOp {
-        op: DslUnaryOp,
-        value: Box<DslValue>,
-    },
-    IntBinOp {
-        op: DslIntBinOp,
-        left: Box<DslValue>,
-        right: Box<DslValue>,
-    },
-    Ternary {
-        condition: Box<DslCondition>,
-        then_value: Box<DslValue>,
-        else_value: Box<DslValue>,
-    },
-    OrigCall(DslOrigArgs),
-    Call(DslCallStmt),
-    NewObject {
-        class_name: String,
-        ctor_sig: Option<String>,
-        args: Vec<DslValue>,
-    },
-    FieldGet {
-        stmt: Box<DslFieldStmt>,
-        is_static: bool,
-    },
-    Cast {
-        value: Box<DslValue>,
-        class_name: String,
-    },
-    ArrayLength(Box<DslValue>),
-    ArrayLiteral {
-        elements: Vec<DslValue>,
-    },
-    ArrayGet {
-        array: Box<DslValue>,
-        index: Box<DslValue>,
-        type_name: Option<String>,
-    },
-}
-
-#[derive(Clone, Copy)]
-pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) enum DslUnaryOp {
-    Neg,
-    BitNot,
-    BoolNot,
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) enum DslIntBinOp {
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Rem,
-    And,
-    Or,
-    Xor,
-    Shl,
-    Shr,
-    Ushr,
-}
-
-#[derive(Clone)]
 pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) enum DslCondition {
     Null {
         value: DslValue,
@@ -237,13 +168,4 @@ pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) enum DslCondition
     And(Box<DslCondition>, Box<DslCondition>),
     Or(Box<DslCondition>, Box<DslCondition>),
     Not(Box<DslCondition>),
-}
-
-#[derive(Clone)]
-pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) enum DslTarget {
-    This,
-    Arg(usize),
-    Last,
-    Result,
-    Local(String),
 }
---
+++ quickjs-hook/src/jsapi/java/java_hook_api/managed_dex_builder/dsl/ast_expr.rs
@@ -0,0 +1,79 @@
+use super::*;
+
+#[derive(Clone)]
+pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) enum DslValue {
+    Target(DslTarget),
+    String(String),
+    Int(i16),
+    Bool(bool),
+    Null,
+    UnaryOp {
+        op: DslUnaryOp,
+        value: Box<DslValue>,
+    },
+    IntBinOp {
+        op: DslIntBinOp,
+        left: Box<DslValue>,
+        right: Box<DslValue>,
+    },
+    Ternary {
+        condition: Box<DslCondition>,
+        then_value: Box<DslValue>,
+        else_value: Box<DslValue>,
+    },
+    OrigCall(DslOrigArgs),
+    Call(DslCallStmt),
+    NewObject {
+        class_name: String,
+        ctor_sig: Option<String>,
+        args: Vec<DslValue>,
+    },
+    FieldGet {
+        stmt: Box<DslFieldStmt>,
+        is_static: bool,
+    },
+    Cast {
+        value: Box<DslValue>,
+        class_name: String,
+    },
+    ArrayLength(Box<DslValue>),
+    ArrayLiteral {
+        elements: Vec<DslValue>,
+    },
+    ArrayGet {
+        array: Box<DslValue>,
+        index: Box<DslValue>,
+        type_name: Option<String>,
+    },
+}
+
+#[derive(Clone, Copy)]
+pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) enum DslUnaryOp {
+    Neg,
+    BitNot,
+    BoolNot,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) enum DslIntBinOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Rem,
+    And,
+    Or,
+    Xor,
+    Shl,
+    Shr,
+    Ushr,
+}
+
+#[derive(Clone)]
+pub(in crate::jsapi::java::java_hook_api::managed_dex_builder) enum DslTarget {
+    This,
+    Arg(usize),
+    Last,
+    Result,
+    Local(String),
+}
